724e4960b5ba6e36f2f04141e5a5f1752e35d9b4
Lingchen Zhu <zhulingchen@gmail.com>
2015-01-11 20:22:22 -0500

    added -O3 optimization flag for gcc to reduce running time; added an example of writing C/Mex code with OpenMP (will be removed later)

---
+++ src/fd_cmex/test_OpenMp.c
@@ -0,0 +1,26 @@
+#include "mex.h"
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define N       1000000
+
+void mexFunction(int nlhs,mxArray* plhs[], int nrhs, const mxArray* prhs[])
+{
+    int i, nthreads, tid;
+    double a[N], sum_result;
+    
+    /* Some initializations */
+    for (i=0; i < N; i++)
+        a[i] = i;
+    sum_result = 0.0;
+
+    #pragma omp parallel for reduction(+:sum_result)
+        for (i=0; i < N; i++)
+        {
+            sum_result += a[i];
+            //mexPrintf("Added %f, Sum = %f\n", a[i], sum_result);
+        }
+
+    //mexPrintf("Sum = %f\n", sum_result);
+    
+}
