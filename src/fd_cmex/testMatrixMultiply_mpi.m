af16f94620eba7aa083768ba5a51fa77c8216ee6
Lingchen Zhu <zhulingchen@gmail.com>
2015-01-16 14:10:44 -0500

    only leave the output of the master process and suppress the outputs of all worker processes by passing out the ranker ids of all parallel Matlab processes, because we cannot avoid running the Matlab wrapper many times concurrently to achieve MPI

--- src/fd_cmex/testMatrixMultiply_mpi.m
+++ src/fd_cmex/testMatrixMultiply_mpi.m
@@ -2,14 +2,18 @@ close all;
 clear;
 clc;
 
+MASTER = 0;
+
 % interface for testMatrixMultiply_mpiAgent
 A = randn(256, 512);
 B = randn(512, 256);
 C = A * B;
-C_mpi = testMatrixMultiply_mpiAgent(A, B);
-[m, n] = size(C_mpi);
-fprintf('m = %d, n = %d\n', m, n);
-delta = C - C_mpi;
-max(abs(delta(:)))
+[C_mpi, taskId] = testMatrixMultiply_mpiAgent(A, B);
+if (taskId == MASTER)
+    [m, n] = size(C_mpi);
+    fprintf('(In Matlab interface) Master: m = %d, n = %d, taskId = %d\n', m, n, taskId);
+    delta = C - C_mpi;
+    max(abs(delta(:)))
+end
 
 quit;
--- src/fd_cmex/testMatrixMultiply_mpiAgent.c
+++ src/fd_cmex/testMatrixMultiply_mpiAgent.c
@@ -7,6 +7,7 @@
 #define A_IN        prhs[0] /* (M * P) */
 #define B_IN        prhs[1] /* (P * N) */
 #define C_OUT       plhs[0] /* (M * N) */
+#define TASKID_OUT  plhs[1]
 /* MPI related */
 #define MASTER      0       /* taskid of first task */
 #define FROM_MASTER	1       /* self-defined message tag */
@@ -44,7 +45,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     errorCode = MPI_Init(NULL, NULL);
     if (errorCode != MPI_SUCCESS)
     {
-        mexPrintf("Error starting MPI program, terminating...\n");
+        mexPrintf("MPI System: Error starting MPI program, terminating...\n");
         MPI_Abort(MPI_COMM_WORLD, errorCode);
     }
     /* find out number of processes */
@@ -54,7 +55,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     
     if (numTasks < 2)
     {
-        mexPrintf("Need at least two MPI tasks, terminating...\n");
+        mexPrintf("MPI System: Need at least two MPI tasks, terminating...\n");
         MPI_Abort(MPI_COMM_WORLD, errorCode);
     }
     numWorkers = numTasks - 1;
@@ -66,10 +67,10 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         pB = mxGetPr(B_IN);
         pC = (double*)mxCalloc(M * N, sizeof(double));
         // test begin
-        mexPrintf("output: pA[0] = %f, pB[0] = %f\n", pA[0], pB[0]);
+        mexPrintf("master (test output): pA[0] = %f, pB[0] = %f\n", pA[0], pB[0]);
         // test end
         
-        mexPrintf("MPI started with %d tasks, including %d workers\n", numTasks, numWorkers);
+        mexPrintf("MPI System: started with %d tasks, including %d workers\n", numTasks, numWorkers);
         avgCol = N / numWorkers;
         extra = N % numWorkers;
         offset = 0;
@@ -80,22 +81,22 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
             cols = (dest <= extra) ? (avgCol + 1) : (avgCol); /* number of columns processed by current worker */
             MPI_Send(&offset, 1, MPI_INT, dest, msgType, MPI_COMM_WORLD);
             // test begin
-            mexPrintf("output: master sent offset = %d to worker %d\n", offset, dest);
+            mexPrintf("master (test output): sent offset = %d to worker %d\n", offset, dest);
             // test end
             MPI_Send(&cols, 1, MPI_INT, dest, msgType, MPI_COMM_WORLD);
             // test begin
-            mexPrintf("output: master sent cols = %d to worker %d\n", cols, dest);
+            mexPrintf("master (test output): sent cols = %d to worker %d\n", cols, dest);
             // test end
             MPI_Send(pA, M * P, MPI_DOUBLE, dest, msgType, MPI_COMM_WORLD);
             // test begin
-            mexPrintf("output: master sent matrix A (%d * %d) to worker %d\n", M, P, dest);
-            mexPrintf("output: pA[0] = %f, pA[%d * %d - 1] = %f sent\n", pA[0], M, P, pA[M * P - 1]);
+            mexPrintf("master (test output): sent matrix A (%d * %d) to worker %d\n", M, P, dest);
+            mexPrintf("master (test output): pA[0] = %f, pA[%d * %d - 1] = %f sent\n", pA[0], M, P, pA[M * P - 1]);
             // test end
-            MPI_Send(pB + offset * P, P * cols, MPI_DOUBLE, dest, msgType, MPI_COMM_WORLD); /* Matlab is using column indexing */
+            MPI_Send(pB + P * offset, P * cols, MPI_DOUBLE, dest, msgType, MPI_COMM_WORLD); /* Matlab is using column indexing */
             // test begin
-            mexPrintf("output: pB[%d * %d] = %f, pB[%d * %d + %d * %d - 1] = %f sent\n", offset, P, pB[offset * P], offset, P, P, cols, pB[offset * P + P * cols - 1]);
+            mexPrintf("master (test output): pB[%d * %d] = %f, pB[%d * %d + %d * %d - 1] = %f sent\n", P, offset, pB[P * offset], P, offset, P, cols, pB[P * offset + P * cols - 1]);
             // test end
-            mexPrintf("Master sent %d columns of matrix B to Worker %d, offset = %d\n", cols, dest, offset);
+            mexPrintf("Master: sent %d columns of matrix B to Worker %d, offset = %d\n", cols, dest, offset);
             offset += cols;
         }
         /* receive results from workers */
@@ -104,17 +105,17 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         {
             MPI_Recv(&offset, 1, MPI_INT, source, msgType, MPI_COMM_WORLD, &status);
             // test begin
-            mexPrintf("output: master received offset = %d\n", offset);
+            mexPrintf("master (test output): received offset = %d\n", offset);
             // test end
             MPI_Recv(&cols, 1, MPI_INT, source, msgType, MPI_COMM_WORLD, &status);
             // test begin
-            mexPrintf("output: master received cols = %d\n", cols);
+            mexPrintf("master (test output): received cols = %d\n", cols);
             // test end
             MPI_Recv(pC + offset * M, M * cols, MPI_DOUBLE, source, msgType, MPI_COMM_WORLD, &status);
             // test begin
-            mexPrintf("output: pC[%d * %d] = %f received\n", offset, M, pC[offset * M]);
+            mexPrintf("master (test output): pC[%d * %d] = %f received\n", offset, M, pC[offset * M]);
             // test end
-            mexPrintf("Master received %d columns of result matrix C from Worker %d\n", cols, source);
+            mexPrintf("Master: received %d columns of result matrix C from Worker %d\n", cols, source);
         }
     }
     /* workers */
@@ -124,26 +125,26 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         msgType = FROM_MASTER;
         MPI_Recv(&offset, 1, MPI_INT, MASTER, msgType, MPI_COMM_WORLD, &status);
         // test begin
-        mexPrintf("output: worker %d received offset = %d from master\n", taskId, offset);
+        mexPrintf("worker %d (test output): received offset = %d from master\n", taskId, offset);
         // test end
         MPI_Recv(&cols, 1, MPI_INT, MASTER, msgType, MPI_COMM_WORLD, &status);
         // test begin
-        mexPrintf("output: worker %d received cols = %d from master\n", taskId, cols);
+        mexPrintf("worker %d (test output): received cols = %d from master\n", taskId, cols);
         // test end
         pA = (double*)mxCalloc(M * P, sizeof(double));
         MPI_Recv(pA, M * P, MPI_DOUBLE, MASTER, msgType, MPI_COMM_WORLD, &status);
         // test begin
-        mexPrintf("output: worker %d received matrix A (%d * %d) from master\n", taskId, M, P);
-        mexPrintf("output: pA[0] = %f, pA[%d * %d - 1] = %f received by worker %d\n", pA[0], M, P, pA[M * P - 1], taskId);
+        mexPrintf("worker %d (test output): received matrix A (%d * %d) from master\n", taskId, M, P);
+        mexPrintf("worker %d (test output): pA[0] = %f, pA[%d * %d - 1] = %f received\n", taskId, pA[0], M, P, pA[M * P - 1]);
         // test end
         pB = (double*)mxCalloc(P * cols, sizeof(double));
         MPI_Recv(pB, P * cols, MPI_DOUBLE, MASTER, msgType, MPI_COMM_WORLD, &status);
         // test begin
-        mexPrintf("output: pB[0] = %f, pB[%d * %d - 1] = %f received by worker %d\n", pB[0], P, cols, pB[P * cols - 1], taskId);
+        mexPrintf("worker %d (test output): pB[0] = %f, pB[%d * %d - 1] = %f received\n", taskId, pB[0], P, cols, pB[P * cols - 1]);
         // test end
-        mexPrintf("Worker %d received %d columns of matrix B from master\n", taskId, cols);
+        mexPrintf("Worker %d: received %d columns of matrix B from master\n", taskId, cols);
         // test begin
-        mexPrintf("output: calculating multiplication of (%d * %d) with (%d * %d) matrices on worker %d\n", M, P, P, cols, taskId);
+        mexPrintf("worker %d (test output): calculating multiplication of (%d * %d) with (%d * %d) matrices\n", taskId, M, P, P, cols);
         // test end
         pC = (double*)mxCalloc(M * cols, sizeof(double));
         for (i = 0; i < M; i++)
@@ -151,7 +152,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
                 for (k = 0; k < P; k++)
                     pC[j * M + i] += pA[k * M + i] * pB[j * P + k];
         // test begin
-        mexPrintf("output: pC[0] = %f on worker %d\n", pC[0], taskId);
+        mexPrintf("worker %d (test output): pC[0] = %f\n", taskId, pC[0]);
         // test end
         
         /* send results to master */
@@ -159,7 +160,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         MPI_Send(&offset, 1, MPI_INT, MASTER, msgType, MPI_COMM_WORLD);
         MPI_Send(&cols, 1, MPI_INT, MASTER, msgType, MPI_COMM_WORLD);
         MPI_Send(pC, M * cols, MPI_DOUBLE, MASTER, msgType, MPI_COMM_WORLD);
-        mexPrintf("Worker %d sent %d cols of the matrix multiplication result to Master\n", taskId, cols);
+        mexPrintf("Worker %d: sent %d cols of the matrix multiplication result to Master\n", taskId, cols);
         
         /* free memory */
         mxFree(pA);
@@ -171,16 +172,18 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     MPI_Finalize();
     
     /* output */
-    C_OUT = mxCreateDoubleMatrix(M, N, mxREAL);
+    C_OUT = mxCreateNumericMatrix(0, 0, mxDOUBLE_CLASS, mxREAL);
     if (taskId == MASTER)
     {
         mxSetPr(C_OUT, pC);
+        mxSetM(C_OUT, M);
+        mxSetN(C_OUT, N);
         // test begin
-        mexPrintf("output: final size of C_OUT is (%d * %d) on master (taskId = %d)\n", mxGetM(C_OUT), mxGetN(C_OUT), taskId);
+        mexPrintf("master (taskId = %d) (test output): final size of C_OUT is (%d * %d)\n", taskId, mxGetM(C_OUT), mxGetN(C_OUT));
         // test end
     }
-    
-
+    TASKID_OUT = mxCreateNumericMatrix(1, 1, mxUINT8_CLASS, mxREAL);
+    *((int*)mxGetData(TASKID_OUT)) = taskId;
     
     /* exit Matlab */
     /*mexCallMATLAB(0, 0, 0, 0, "exit");*/
