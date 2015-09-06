f23000846d8e4a9fe0a0cc1adf69a224b709c047
Lingchen Zhu <zhulingchen@gmail.com>
2015-09-06 14:36:41 -0400

    changed the way to randomize data for dimensionality reduction, so that we can directly manipulate the received signal without the need of forward modeling again

--- src/dict_learning_toolbox/test_cs.m
+++ src/dict_learning_toolbox/test_cs.m
@@ -21,12 +21,29 @@ tmpFreq(20:20:80) = 1;
 idctMat = idct(eye(N, N));
 tmpTime = idct(tmpFreq, N);
 
+% BP (time domain)
 tmpFreq_rec_bpdn = spg_bp(csMat * idctMat, csMat * tmpTime);
 figure; plot(real(tmpFreq_rec_bpdn));
-tmpFreq_rec_lasso = spg_lasso(csMat * idctMat, csMat * tmpTime, norm(tmpFreq, 1) * 1.2);
+% LASSO (time domain)
+tmpFreq_rec_lasso = spg_lasso(csMat * idctMat, csMat * tmpTime, norm(tmpFreq, 1));
 figure; plot(real(tmpFreq_rec_lasso));
 
+% BP (freq domain)
 tmpFreq_rec_bpdn = spg_bp(csMat, csMat * tmpFreq);
 figure; plot(real(tmpFreq_rec_bpdn));
-tmpFreq_rec_lasso = spg_lasso(csMat, csMat * tmpFreq, norm(tmpFreq, 1) * 1.2);
+% LASSO (freq domain)
+tmpFreq_rec_lasso = spg_lasso(csMat, csMat * tmpFreq, norm(tmpFreq, 1));
 figure; plot(real(tmpFreq_rec_lasso));
+
+% PQN
+% Set up Objective Function
+csMat = randn(20, 100);
+funObj = @(w)SquaredError(w,csMat * idctMat, csMat * tmpTime);
+% Set up L1-Ball Projection
+tau = 4;
+funProj = @(w)sign(w).*projectRandom2C(abs(w),tau);
+
+fprintf('\nComputing optimal Lasso parameters...\n');
+wL1 = zeros(N, 1);
+[wL1, r] = minConF_PQN_new(funObj,wL1,funProj);
+figure; plot(real(wL1));
